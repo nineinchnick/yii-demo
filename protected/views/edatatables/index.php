bd02e14cb482e34160cbf0bbf944da405453609b
John Was <janek.jan@gmail.com>
2013-09-13 13:34:02 +0200

    moved content from edatatables demo md page into index view

--- protected/views/edatatables/index.php
+++ protected/views/edatatables/index.php
@@ -1,1 +1,15 @@
+<h1>EDataTables extension</h1>
+
+<p>CGridView replacement.</p>
+
+<h2>Resources</h2>
+
+<ul>
+	<li><p><a href="https://github.com/nineinchnick/edatatables">GitHub repo</a>.</p></li>
+	<li><p><a href="http://www.yiiframework.com/extension/edatatables">Extension page</a>.</p></li>
+	<li><p>Available as composer package under <a href="https://packagist.org/packages/nineinchnick/edatatables">nineinchnick/edatatables</a> name.</p></li>
+</ul>
+
+<h2>Demo</h2>
+
 <?php $widget->run();
